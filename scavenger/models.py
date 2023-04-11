b5ad5f99b434c7ce838ff5439b7538959ecee565
MatthewPhan <MatthewPhan06@gmail.com>
2023-04-11 16:12:04 +0800

    Update to QR Generator

--- scavenger/models.py
+++ scavenger/models.py
@@ -7,11 +7,11 @@ from PIL import Image, ImageDraw
 # Create your models here.
 
 class Location(models.Model):
+
     location = models.CharField(max_length=100)
     code = models.ImageField(blank=True, upload_to='static/qrcodes')
 
     def save(self, *args, **kwargs):
-
         # Generate QR
         qr_image = qrcode.make(self.location) 
         qr_offset = Image.new('RGB', (310, 310), 'white')
@@ -24,3 +24,7 @@ class Location(models.Model):
         self.code.save(files_name, File(stream), save=False)
         qr_offset.close()
         super().save(*args, **kwargs)
+
+    # Tells Django Admin to print the instance of the location field of the model 
+    def __str__(self):
+        return self.location
